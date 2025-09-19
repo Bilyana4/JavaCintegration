ba79db97f6124017197a5c5a7290acb829a0b001
Bilyana4 <bilyana03b@gmail.com>
2025-09-19 13:51:37 +0300

    Update FileOperations.java

--- FileOperations.java
+++ FileOperations.java
@@ -1,15 +1,21 @@
-import java.io.File;  // Import the File class
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileOperations {
+    static int size = 0;
+
     public static int Opener(String pathToFile) {
         int linesRead = 0;
 
         try{
             File file = new File(pathToFile);
-            Scanner inputFile = new Scanner(file);
+            Scanner inputFileRead = new Scanner(file);
+            Scanner inputFileDec = new Scanner(file);
 
-            linesRead = Reader(inputFile);
+            linesRead = Reader(inputFileRead);
+            DeconstructFile(inputFileDec, linesRead);
+
             return linesRead;
         } catch (Exception e){
             return -1;
@@ -17,17 +23,44 @@ public class FileOperations {
 
     }
 
-    private static int Reader(Scanner Reader){
+    private static int Reader(Scanner File){
         int lines = 0;
 
-        while (Reader.hasNextLine()) {
-            String line = Reader.nextLine();
+        while (File.hasNextLine()) {
+            String line = File.nextLine();
+
             System.out.println(line);
             lines++;
         }
 
-        Reader.close();
+        File.close();
         return lines;
+    }
+
+    private static void DeconstructFile(Scanner File, int lines){
+        int i = 0;
+        float[] constants = new float[lines];
+
+        size = lines;
+
+        while (File.hasNextLine()){
+
+            String line = File.nextLine();
+
+            if(line.contains(" ")){
+                // За матрица на неизвестните
+                float[] array = NumbersOperations.CreateArrayUnknowns(line, lines);
+                NumbersOperations.CreateMatrix(array, 0);
+            }else{
+                // За корените на уравненията
+                constants[i] = NumbersOperations.CreateArrayConstants(line);
+                //System.out.println(constants[i]);
+                i++;
+            }
+
+        }
+
+        NumbersOperations.CreateMatrix(constants, 1);
     }
 
     public static int CreateFile(String directoryPath){
@@ -45,5 +78,26 @@ public class FileOperations {
             return -1;
         }
         return -1;
+    }
+
+    public static int WriteRootsInFile(String directoryPath) throws FileNotFoundException {
+        try {
+            PrintWriter outputFile = new PrintWriter(directoryPath +"roots.txt");
+
+            int len = (Main.Roots).length;
+
+            for(int i = 0; i < len; i++){
+                outputFile.println(Main.Roots[i]);
+            }
+
+            outputFile.close();
+
+            return 0;
+        } catch (IOException e) {
+            System.out.println("An error occurred!");
+            e.printStackTrace();
+
+            return 1;
+        }
     }
 }
